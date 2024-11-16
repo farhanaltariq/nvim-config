ada60577c5f65f3263429d2ffe56f186bd329e58
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-17 00:50:16 +0700

    show hidden files but hide in search text

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,12 +8,14 @@ return {
     local telescope = require("telescope.builtin")
     vim.keymap.set('n', '<leader>p', function()
       telescope.find_files({
-        hidden = true,  -- Include hidden files
+        hidden = true, -- Include hidden files
+				no_ignore = true,
       })
     end)
     vim.keymap.set('n', '<leader>fg', function()
       telescope.live_grep({
-        hidden = true,  -- Include hidden files
+        hidden = false, -- Include hidden files
+				no_ignore = false,
       })
     end)
   end

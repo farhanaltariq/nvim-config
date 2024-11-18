9ade8fe4c38162bcb4fd0e01f845101c49b79096
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-18 15:35:09 +0700

    formatter, add desc

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,42 +14,47 @@ vim.opt.number = true
 vim.g.mapleader = "\\"
 
 -- Neotree Toggle
-vim.keymap.set('n', '<leader>e', ':Neotree<CR>', {})
+vim.keymap.set('n', '<leader>e', ':Neotree<CR>', { desc = "Explorer" })
 
 -- Exit all
-vim.keymap.set('n', '<leader>q', ':qa<CR>', {})
+vim.keymap.set('n', '<leader>q', ':qa<CR>', { desc = "Exit Nvim" })
 
--- Save file
-vim.keymap.set('n', '<leader>s', ':w<CR>', {})
+-- Format and Save file
+vim.keymap.set('n', '<leader>f', function()
+	vim.lsp.buf.format({ async = true })
+end, { noremap = true, silent = true, desc = "Format File" })
+
+vim.keymap.set('n', '<leader>s', ':w<CR>', { desc = "Save File" })
 
 -- Redo - u for undo
-vim.keymap.set('n', '<S-u>', '<C-r>', {})
+vim.keymap.set('n', '<S-u>', '<C-r>', { desc = "Redo" })
 
 -- Move around files
-vim.keymap.set('n', '<leader><Tab>', ':tabnext<CR>', {})
-vim.keymap.set('n', '<leader><S-Tab>', ':tabprev<CR>', {})
-vim.keymap.set('n', '<leader>t', ':tabnew<CR>', {})
-vim.keymap.set('n', '<leader>w', ':tabclose<CR>', {})
+vim.keymap.set('n', '<leader><Tab>', ':tabnext<CR>', { desc = "Tab Next" })
+vim.keymap.set('n', '<leader><S-Tab>', ':tabprev<CR>', { desc = "Tab Prev" })
+vim.keymap.set('n', '<leader>t', ':tabnew<CR>', { desc = "Tab New" })
+vim.keymap.set('n', '<leader>w', ':tabclose<CR>', { desc = "Tab Close" })
 
 -- Comment a line
-vim.keymap.set('n', '<leader>/', function() require('Comment.api').toggle.linewise.current() end, {})
+vim.keymap.set('n', '<leader>/', function() require('Comment.api').toggle.linewise.current() end, { desc = "Comment line" })
 -- Visual mode: Toggle comment on selected lines
-vim.keymap.set('v', '<leader>/', "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", {})
+vim.keymap.set('v', '<leader>/', "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+	{ desc = "Comment line" })
 
 -- Git shortcut
-vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>', {})
+vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>', { desc = "Git" })
 
 -- SelectAll
-vim.keymap.set('n', '<leader>a', 'ggVG', {})
+vim.keymap.set('n', '<leader>a', 'ggVG', { desc = "Select All" })
 
 -- Mapping for LSP go to definition and go to implementation
-vim.api.nvim_set_keymap('n', '<leader>[', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>]', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>[', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true, desc="Go to definition" })
+vim.api.nvim_set_keymap('n', '<leader>]', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true, desc="Go to implementation" })
 
--- Shortcut untuk minimize (hide) dari dalam Floaterm
-vim.keymap.set("t", "<F10>", "<C-\\><C-n>:FloatermHide<CR>", { desc = "Toggle Floaterm", silent = true })
-vim.keymap.set("n", "<F10>", ":FloatermToggle<CR>", { desc = "Hide Floaterm", silent = true })
-vim.keymap.set("i", "<F10>", "<ESC>:FloatermToggle<CR>", { desc = "Hide Floaterm", silent = true })
-vim.keymap.set("x", "<F10>", "<ESC>:FloatermToggle<CR>", { desc = "Hide Floaterm", silent = true })
+-- Shortcut untuk minimize (hide) dari dala Terminalm
+vim.keymap.set("t", "<F10>", "<C-\\><C-n>:FloatermHide<CR>", { desc = "Toggle Terminal", silent = true })
+vim.keymap.set("n", "<F10>", ":FloatermToggle<CR>", { desc = "Hide Terminal", silent = true })
+vim.keymap.set("i", "<F10>", "<ESC>:FloatermToggle<CR>", { desc = "Hide Terminal", silent = true })
+vim.keymap.set("x", "<F10>", "<ESC>:FloatermToggle<CR>", { desc = "Hide Terminal", silent = true })
 
-vim.keymap.set("t", "<F9>", "<C-\\><C-n>:FloatermNext<CR>", { desc = "Toggle Floaterm", silent = true })
+vim.keymap.set("t", "<F9>", "<C-\\><C-n>:FloatermNext<CR>", { desc = "Change Terminal", silent = true })
--- lua/plugins/masonlsp.lua
+++ lua/plugins/masonlsp.lua
@@ -1,27 +1,54 @@
 -- Mason and LSP Config
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      -- Mason setup
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"gopls", "pbls", "rust_analyzer"},
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    -- LSP setup
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			-- Mason setup
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "gopls", "pbls", "rust_analyzer" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		-- LSP setup
+		config = function()
+			local lspconfig = require('lspconfig')
+			-- Lua LSP setup
+			lspconfig.lua_ls.setup({})
+			-- Go LSP setup with formatting
+			lspconfig.gopls.setup({
+				on_attach = function(client, bufnr)
+					-- Enable formatting with gofmt
+					if client.server_capabilities.documentFormattingProvider then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							buffer = bufnr,
+							callback = function()
+								vim.cmd("silent! GoFmt")
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local null_ls = require("null-ls")
+			-- Configure gofmt with null-ls
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.gofmt,
+				},
+			})
+		end,
+	}
 }

56c930411756e7c77e2be7c209cfcde9c39b2d8e
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-18 21:22:56 +0700

    get rid of warning

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,16 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local telescope = require("telescope.builtin")
-		local wk = require("which-key")
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local telescope = require("telescope.builtin")
+    local wk = require("which-key")
 
-		-- Register keymaps for Telescope
-		wk.register({
-			p = { function() telescope.find_files({ hidden = true, no_ignore = true }) end, "Find Files" },
-			F = { function() telescope.live_grep({ hidden = false, no_ignore = false }) end, "Find Everywhere" }
-		}, { prefix = "<leader>" })
-	end
+    -- Register keymaps for Telescope
+    wk.add({
+      { '<leader>p', function() telescope.find_files({ hidden = true, no_ignore = true }) end, group = 'Find Files' },
+      { '<leader>F', function() telescope.live_grep({ hidden = false, no_ignore = false }) end, group = 'Find Everywhere' }
+    })
+  end
 }
 

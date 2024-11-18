6fd8f8f677d1c9356a1fddbfe1ffe52063f3fd9d
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-18 21:31:23 +0700

    larger preview

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,25 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local telescope = require("telescope.builtin")
-    local wk = require("which-key")
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local telescope = require("telescope.builtin")
+		local wk = require("which-key")
 
-    -- Register keymaps for Telescope
-    wk.add({
-      { '<leader>p', function() telescope.find_files({ hidden = true, no_ignore = true }) end, group = 'Find Files' },
-      { '<leader>F', function() telescope.live_grep({ hidden = false, no_ignore = false }) end, group = 'Find Everywhere' }
-    })
-  end
+		-- Register keymaps for Telescope
+		wk.add({
+			{ '<leader>p', function() telescope.find_files({ hidden = true, no_ignore = true }) end,  group = 'Find Files' },
+			{ '<leader>F', function() telescope.live_grep({ hidden = false, no_ignore = false }) end, group = 'Find Everywhere' },
+		})
+
+		-- Configure Telescope to make preview window larger
+		require('telescope').setup {
+			defaults = {
+				layout_config = {
+					preview_width = 0.55,     -- Adjust this value for the preview window size (larger than the search list)
+					prompt_position = "bottom", -- Optional: places the prompt at the top
+				},
+			},
+		}
+	end
 }
-

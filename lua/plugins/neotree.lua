912b0240ce46a756dced159471867600c3c427d8
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-19 21:35:01 +0700

    disable wrap text when expanding neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,6 +9,7 @@ return {
 	},
 	config = function()
 		-- NeoTree - file explorer
+		vim.wo.wrap = false -- Set wrapper text when expanding neotree
 		require("neo-tree").setup({
 			filesystem = {
 				filtered_items = {
@@ -26,6 +27,7 @@ return {
 			},
 		})
 
+		-- OPEN NEOTREE AT OPENING NVIM
 		-- vim.api.nvim_create_augroup("neotree", {})
 		-- vim.api.nvim_create_autocmd("UiEnter", {
 		-- 	desc = "Open Neotree automatically",
@@ -38,30 +40,3 @@ return {
 		-- })
 	end
 }
-  -- opts = {
-  --   filesystem = {
-  --     commands = {
-  --       -- If item is a file it will close neotree after opening it.
-  --       open_and_close_neotree = function(state)
-  --         require("neo-tree.sources.filesystem.commands").open(state)
-  --
-  --         local tree = state.tree
-  --         local success, node = pcall(tree.get_node, tree)
-  --
-  --         if not success then
-  --           return
-  --         end
-  --
-  --         if node.type == "file" then
-  --           require("neo-tree.command").execute({ action = "close" })
-  --         end
-  --       end,
-  --     },
-  --     window = {
-  --       mappings = {
-  --         ["<CR>"] = "open_and_close_neotree",
-  --         ["<S-CR>"] = "open",
-  --       },
-  --     },
-  --   },
-  -- },

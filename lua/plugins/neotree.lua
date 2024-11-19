c288857860530e7fc29923cda211cbc07d243ba9
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-19 22:10:21 +0700

    close neotree when opening file
    close neotree when opening files and show notification popup when files
    moved or renamed

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -13,16 +13,41 @@ return {
 		require("neo-tree").setup({
 			filesystem = {
 				filtered_items = {
-					visible = true,          -- Show hidden files
-					hide_dotfiles = false,   -- Don't hide dotfiles
+					visible = true,     -- Show hidden files
+					hide_dotfiles = false, -- Don't hide dotfiles
 					hide_gitignored = false, -- Optionally, show git-ignored files too
 				},
 				window = {
 					position = "right", -- You can adjust the position
-					width = 40,         -- Set the width of the Neo-tree panel
+					width = 40,    -- Set the width of the Neo-tree panel
 					mappings = {
 						["o"] = "",
 					}
+				},
+			},
+			event_handlers = {
+				{
+					event = "file_open_requested",
+					handler = function()
+						-- auto close
+						-- vim.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end
+				},
+				{
+					event = "file_renamed",
+					handler = function(args)
+						-- fix references to file
+						print(args.source, " renamed to ", args.destination)
+					end
+				},
+				{
+					event = "file_moved",
+					handler = function(args)
+						-- fix references to file
+						print(args.source, " moved to ", args.destination)
+					end
 				},
 			},
 		})

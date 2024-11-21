9c3400a7a885dd8e03e091436389fa97987a7775
Farhan Altariq <farhanaaxy@gmail.com>
2024-11-21 23:47:03 +0700

    show what is recording

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,28 +1,49 @@
 -- Lualine (Status Line)
-  return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup({
-        options = {
-          theme = 'nord',
-					section_separators = {'', ''},
-          component_separators = {'', ''},
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch'},
-          lualine_c = {
-						{
-							'filename',
-							path = 1
-						}  -- Use path = 1 for relative path, 2 for absolute path
+return {
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons', "codota/tabnine-nvim" },
+	config = function()
+		local tabnine = require("tabnine.status")
+		require('lualine').setup({
+			options = {
+				theme = 'OceanicNext',
+				section_separators = { '', '' },
+				component_separators = { '', '' },
+			},
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = {
+					'branch',
+					{
+						function()
+							return vim.fn.reg_recording() ~= '' and 'Recording @ ' .. vim.fn.reg_recording() or ''
+						end,
+						cond = function()
+							return vim.fn.reg_recording() ~= ''
+						end,
+						color = { fg = '#ff9e64', gui = 'bold' },
 					},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'},
-        },
-        extensions = {'neo-tree'},
-      })
-    end,
-  }
+					'showcmd',
+				},
+				lualine_c = {
+					{
+						'filename',
+						path = 1
+					} -- Use path = 1 for relative path, 2 for absolute path
+				},
+				lualine_x = {
+					'searchcount',
+					'encoding',
+					'fileformat',
+					'filetype',
+					function()
+						return tabnine.status()
+					end,
+				},
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' },
+			},
+			extensions = { 'neo-tree' },
+		})
+	end,
+}

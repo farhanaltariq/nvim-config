f8b18b03dd0eef49c33bb360728434f1587db800
Farhan Altariq <farhanaaxy@gmail.com>
2024-12-12 13:50:09 +0700

    setup debugger, setup go debug

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dapui.setup()
+
+			-- Debugger mappings
+			vim.keymap.set('n', '<F5>', function() dap.continue() end, { desc = "Continue Debugging" })
+			vim.keymap.set('n', '<F10>', function() dap.step_over() end, { desc = "Step Over" })
+			vim.keymap.set('n', '<F11>', function() dap.step_into() end, { desc = "Step Into" })
+			vim.keymap.set('n', '<F12>', function() dap.step_out() end, { desc = "Step Out" })
+			vim.keymap.set('n', '<M-b>', function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+			vim.keymap.set({ 'n', 'v' }, '<M-e>', function() dapui.eval() end, { desc = "Evaluate Expression" })
+			vim.keymap.set('n', '<M-w>', function() require('dapui').elements.watches.add() end, { desc = "Add Watch" })
+			vim.keymap.set('n', '<M-W>', function() require('dapui').elements.watches.remove() end, { desc = "Remove Watch" })
+
+			-- Automatically open/close Dap UI
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+
+			-- Close Dap UI command
+			vim.api.nvim_create_user_command("DapCloseUI", function()
+				dapui.close()
+			end, {})
+		end,
+	},
+	{
+		"leoluz/nvim-dap-go",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("dap-go").setup()
+		end,
+	},
+}

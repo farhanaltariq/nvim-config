0f6a504cb5fe2eff76ca38b90c5af391a645ed17
Farhan Altariq <farhanaaxy@gmail.com>
2024-12-16 11:35:06 +0700

    Revert "change to leader key"
    This reverts commit a3e2df424731675f0ea66f548f1b569d5861836f.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -14,11 +14,10 @@ return {
 			vim.keymap.set('n', '<F10>', function() dap.step_over() end, { desc = "Step Over" })
 			vim.keymap.set('n', '<F11>', function() dap.step_into() end, { desc = "Step Into" })
 			vim.keymap.set('n', '<F12>', function() dap.step_out() end, { desc = "Step Out" })
-			vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
-			vim.keymap.set({ 'n', 'v' }, '<leader>e', function() dapui.eval() end, { desc = "Evaluate Expression" })
-			vim.keymap.set('n', '<leader>w', function() require('dapui').elements.watches.add() end, { desc = "Add Watch" })
-			vim.keymap.set('n', '<leader>W', function() require('dapui').elements.watches.remove() end,
-				{ desc = "Remove Watch" })
+			vim.keymap.set('n', '<M-b>', function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+			vim.keymap.set({ 'n', 'v' }, '<M-e>', function() dapui.eval() end, { desc = "Evaluate Expression" })
+			vim.keymap.set('n', '<M-w>', function() require('dapui').elements.watches.add() end, { desc = "Add Watch" })
+			vim.keymap.set('n', '<M-W>', function() require('dapui').elements.watches.remove() end, { desc = "Remove Watch" })
 
 			-- Automatically open/close Dap UI
 			dap.listeners.before.attach.dapui_config = function()

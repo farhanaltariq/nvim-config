c6de19fb0f92c43b639a669330f32580040b4d49
Farhan Altariq <farhanaaxy@gmail.com>
2025-04-14 14:29:01 +0700

    remap multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,7 +1,13 @@
 return {
 	"mg979/vim-visual-multi",
-	event = 'VeryLazy',
-	config = function()
-		-- You can configure vim-visual-multi if needed
+	lazy = false,                   -- Force immediate load for testing
+	init = function()
+		vim.g.VM_default_mappings = 0 -- Disable ALL defaults
+		vim.g.VM_maps = {
+			['Find Under'] = '<C-d>',   -- Essential starter
+			['Find Subword Under'] = '<C-d>',
+			['Add Cursor Down'] = '<C-j>', -- Using Alt instead of Ctrl
+			['Add Cursor Up'] = '<C-k>',
+		}
 	end
 }

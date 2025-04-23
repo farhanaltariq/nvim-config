9ca6705c09b807cc443e134aae2fab124d3a60f9
Farhan Altariq <farhanaaxy@gmail.com>
2025-04-23 08:14:21 +0700

    Revert "add feature to switch between language"
    This reverts commit d723856688ce42bd0d395d6db7964e98974c34c5.

--- lua/plugins/masonlsp.lua
+++ lua/plugins/masonlsp.lua
@@ -17,25 +17,67 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		-- LSP setup
 		config = function()
-			-- Command to switch languages
-			vim.api.nvim_create_user_command("UseLang", function(opts)
-				require("langs.lang_switcher").switch(opts.args)
-			end, {
-				nargs = 1,
-				complete = function()
-					return require("langs.lang_switcher").list()
+			local lspconfig = require('lspconfig')
+
+			-- Keymap function for convenience
+			local on_attach = function(_, _)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					-- buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+			end
+
+			-- Lua LSP setup
+			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
+			})
+
+			-- Go LSP setup with formatting
+			lspconfig.gopls.setup({
+				settings = {
+					gopls = {
+						staticcheck = true, -- enables linting and more advanced checks
+						analyses = {
+							unusedparams = true, -- enable specific linting rules
+							shadow = true,
+							format = true, -- Make sure format-related checks are enabled
+						},
+						gofumpt = true, -- Use gofumpt for stricter formatting
+						hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							constantValues = true,
+							parameterNames = true,
+						},
+					},
+				},
+				on_attach = function(_, _)
+					-- Disable default LSP diagnostics to allow tiny-inline-diagnostic to handle them
+					vim.diagnostic.config({
+						virtual_text = false, -- Disable inline diagnostic messages
+						signs = true,       -- Disable diagnostic signs in the sign column
+						underline = true,   -- Disable underlining of errors
+						update_in_insert = false, -- Disable updates to diagnostics in insert mode
+					})
 				end,
 			})
 
-			-- Command to list available languages
-			vim.api.nvim_create_user_command("ListLangs", function()
-				local langs = require("langs.lang_switcher").list()
-				print("Available languages:")
-				for _, lang in ipairs(langs) do
-					print(" - " .. lang)
-				end
-			end, {})
+			-- Rust LSP setup
+			lspconfig.rust_analyzer.setup({
+				on_attach = function(_, _)
+					-- Disable default LSP diagnostics to allow tiny-inline-diagnostic to handle them
+					vim.diagnostic.config({
+						virtual_text = false, -- Disable inline diagnostic messages
+						signs = true,       -- Disable diagnostic signs in the sign column
+						underline = true,   -- Disable underlining of errors
+						update_in_insert = false, -- Disable updates to diagnostics in insert mode
+					})
+				end,
+			})
 		end,
 	},
 	{
@@ -44,7 +86,11 @@ return {
 		config = function()
 			local null_ls = require("null-ls")
 			-- Configure gofmt with null-ls
-			null_ls.setup()
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.gofmt,
+				},
+			})
 		end,
 	},
 }
